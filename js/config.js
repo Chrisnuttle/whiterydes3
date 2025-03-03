e1aea24d7c19680f46bb2f5f52a2de481bd67302
Chrisnuttle <chrisnuttle6857@gmail.com>
2025-03-03 13:56:45 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_X49P84nh2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '245q3hnarnkr1drtrk8ofl8an7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'eu-west-2_oK5TKKya0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2v8g24cnrmrceot3vvnfmpu45b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
